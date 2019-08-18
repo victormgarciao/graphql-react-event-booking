bb5f1fa0358393b6d68beca52b0e4cbf6c5ccb06
Victor Garcia <vimago81@gmail.com>
2019-08-18 14:04:07 +0200

    - MongoDb and graphQL connection in app.js with schemas, models, mongoose and so on

---
+++ models/event.js
@@ -0,0 +1,24 @@
+const mongose = require('mongoose');
+
+const Schema = mongose.Schema;
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+});
+
+module.exports = mongose.model('Event', eventSchema);
