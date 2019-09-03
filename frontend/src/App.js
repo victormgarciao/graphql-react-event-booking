76f58090ac946031022a235a6b4f037d4dc6e43a
Victor Garcia <vimago81@gmail.com>
2019-09-03 11:12:51 +0200

    Lesson 12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route, Redirect, Switch }  from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
 import BookingPage from './pages/Bookings';
 import EventPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventPage} />
-          <Route path="/bookings" component={BookingPage} />
-        </Switch>
+        <Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventPage} />
+              <Route path="/bookings" component={BookingPage} />
+            </Switch>
+          </main>
+        </Fragment>
       </BrowserRouter>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
