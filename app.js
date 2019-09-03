cb202f2920d78fb6e0e88762c66d0b760007e81c
Victor Garcia <vimago81@gmail.com>
2019-09-03 12:22:24 +0200

    Lesson 13

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
+
 app.use(isAuth);
 
 app.use('/graphql',
@@ -29,6 +40,6 @@ mongoose.connect(
     `mongodb+srv://${mongoUser}:${mongoPassword}@vicluster-j65o8.mongodb.net/${mongoDB}?retryWrites=true&w=majority`
 )
 .then(() => {
-    app.listen(3000);
+    app.listen(8000);
 })
 .catch(err => console.log(err));
